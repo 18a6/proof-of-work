52a06ab88f07ede6e512a9318707266105c6131c
unknown <ayakhedda98@gmail.com>
2023-08-22 16:14:59 +0100

    initial commit

--- pow.py
+++ pow.py
@@ -1,7 +1,33 @@
+import hashlib
+import sys
+
 class Block:
-    def __init__(self, contents_length, contents_hash,previous_hash):
-        self.contents_length = contents_length
-        self.contents_hash = contents_hash
+    def __init__(self, data, previous_hash):
+        self.nonce = 0
+        self.data = data
         self.previous_hash = previous_hash
+
+    
+    def mine(block, target_hash):
+        for i in range(sys.maxsize):
+            block.nonce = i
+
+            #creating an object for SHA-256
+            sha256 = hashlib.sha256()
+
+            #update the object with the bytes of the variables
+            sha256.update(block.nonce.to_bytes(32, byteorder='big'))
+            sha256.update(block.previous_hash)
+            sha256.update(block.contents_hash)
+
+            #calculate the block's hash
+            blockHash = sha256.hexdigest()
+
+
+            #compare the block's hash to the target hash
+            if(blockHash < target_hash):
+                print(f"The block\'s hash is:{block.nonce}")
+                break
+
 
 
