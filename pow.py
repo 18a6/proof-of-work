24ebd8708563ebe1bf16be8366ccdfe35dcbf07d
Aymen-Tirchi <aymentirchi05@gmail.com>
2023-08-22 15:27:49 +0100

    block header

---
+++ pow.py
@@ -0,0 +1,13 @@
+class Block_header:
+    def __init__(self):
+        self.blockhash = "r1qg715t756v1f7b186bxg7f1v7g"
+        self.timestamp = "2023-08-22 3:17:00"
+        self.data = "pow from aymen"
+        self.prevhash = "hrqlvjbtwvx157t6wrgw6vw"
+
+header = Block_header()
+
+print("Block Hash:", header.blockhash)
+print("Block timestamp:", header.blockhash)
+print("Block data:", header.blockhash)
+print("Block previous Hash:", header.blockhash)
