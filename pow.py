ec93287666dc7529fa82c6781e67f00dfc5c65d8
Aymen-Tirchi <aymentirchi05@gmail.com>
2023-08-23 13:45:51 +0100

    resolved issue

--- pow.py
+++ pow.py
@@ -1,13 +1,18 @@
 class BlockHeader:
-    def __init__(self):
-        self.blockhash = "r1qg715t756v1f7b186bxg7f1v7g"
-        self.timestamp = "2023-08-22 3:17:00"
-        self.data = "pow from aymen"
-        self.prevhash = "hrqlvjbtwvx157t6wrgw6vw"
+    def __init__(self, blockhash, timestamp, data, prevhash):
+        self.blockhash = blockhash
+        self.timestamp = timestamp
+        self.data = data
+        self.prevhash = prevhash
 
-header = BlockHeader()
+block_hash = "0xd7fdfbeaf23834313e3c976e75feca61e95af38f00588a75cb3911073516e4f7"
+timestamp = "2023-08-23 1:00:00"
+data = "pow exercise from solidity study group"
+prev_hash = "0xd7fdfbeaf23834313e3c976e75feca61e95af38f00588a75cb3911073516e4f7"
 
-print("Block Hash:", header.blockhash)
-print("Block timestamp:", header.blockhash)
-print("Block data:", header.blockhash)
-print("Block previous Hash:", header.blockhash)
+block_header = BlockHeader(block_hash, timestamp, data, prev_hash)
+
+print("Block Hash:", block_header.blockhash)
+print("Block timestamp:", block_header.timestamp)
+print("Block data:", block_header.data)
+print("Block previous Hash:", block_header.prevhash)
