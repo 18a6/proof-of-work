3adbc3caad04771aa165bef86e171722d21e7f95
Aymen-Tirchi <aymentirchi05@gmail.com>
2023-08-23 15:59:43 +0100

    added time and hashlib

--- pow.py
+++ pow.py
@@ -1,16 +1,23 @@
+import time
+import hashlib
+
 class BlockHeader:
-    def __init__(self, blockhash, timestamp, data, prevhash):
-        self.blockhash = blockhash
+    def __init__(self, timestamp, data, prevhash):
         self.timestamp = timestamp
         self.data = data
         self.prevhash = prevhash
+        self.blockhash = self.calculate_hash()
 
-block_hash = "0xd7fdfbeaf23834313e3c976e75feca61e95af38f00588a75cb3911073516e4f7"
-timestamp = "2023-08-23 1:00:00"
+    def calculate_hash(self):
+        value = self.timestamp + self.data + self.prevhash
+        return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+current_timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
 data = "pow exercise from solidity study group"
 prev_hash = "0xd7fdfbeaf23834313e3c976e75feca61e95af38f00588a75cb3911073516e4f7"
 
-block_header = BlockHeader(block_hash, timestamp, data, prev_hash)
+block_header = BlockHeader(current_timestamp, data, prev_hash)
 
 print("Block Hash:", block_header.blockhash)
 print("Block timestamp:", block_header.timestamp)
