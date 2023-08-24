b535e8e0d2da2b3b2a5d52e5ec1fc78219ea05f1
18a6 <ar.brhl49@gmail.com>
2023-08-24 12:08:41 +0100

    feat: added mine_block

--- pow.py
+++ pow.py
@@ -1,7 +1,8 @@
+import random
 import hashlib
 
 class Block():
-    __slots__ = 'contents_hash', 'previous_hash'
+    __slots__ = 'contents_hash', 'previous_hash', 'nonce'
     
     def __init__(self, contents_hash='0'*256, previous_hash='0'*256):
         assert type(contents_hash) == str and type(previous_hash) == str
@@ -14,28 +15,47 @@ class Block():
         # self.timestamp
 
         # this is adjusted to make the hash of this header fall in the valid range
-        # self.nonce
+        self.nonce = 0
     
     def build_block(self, previous, contents):
-        block_header = Block()
-        assert isinstance(previous, Block)
+        assert isinstance(previous, Block) or previous == None
 
         if(previous):
             # calculate previous block header hash
             previous_hash = previous.previous_hash
-            block_header.previous_hash = hashlib.sha256(previous_hash.encode('utf-8')).hexdigest()
+            self.previous_hash = hashlib.sha256(previous_hash.encode('utf-8')).hexdigest()
         else:
             # genesis has no previous. just use zeroed hash
-            block_header.previous_hash = 0
+            self.previous_hash = 0
+
+    
         # add data hash
-        block_header.contents_hash = hashlib.sha256(str(contents).encode('utf-8')).hexdigest()
-        print(block_header.contents_hash)
+        self.contents_hash = hashlib.sha256(str(contents).encode('utf-8')).hexdigest()
 
-block_header = Block()
-previous = Block('0'*256, '0'*256)
-block_header.build_block(previous, 0)
+    def mine_block(self):
+        MAX_HASH = 2 ** 6
+        target = hashlib.sha256(str(random.randint(0, MAX_HASH - 1)).encode('utf-8')).hexdigest()
+        # MINING: start of the mining round
+        for nonce in range(MAX_HASH):
+            current_hash = hashlib.sha256((self.contents_hash + str(nonce)).encode('utf-8')).hexdigest()
+            if(current_hash < target):
+                self.nonce = nonce
+                print(nonce)
+                return
+    
+class Blockchain():
+    def __init__(self):
+        self.blocks = []
 
+    def add_block(self, contents):
+        block = Block()
+        if(len(self.blocks) == 0):
+            block.build_block(None, contents)
 
+        else:
+            block.build_block(self.blocks[-1], contents)
+        block.mine_block()
+        self.blocks.append(block)
 
-
-
+blockchain = Blockchain()
+blockchain.add_block('0x0')
