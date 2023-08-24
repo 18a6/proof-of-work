b3b7230b8adfe0706a9cbfe1b3f57ecc1f047c02
18a6 <ar.brhl49@gmail.com>
2023-08-24 08:58:47 +0100

    feat: added condtraints on hash inputs

--- pow.py
+++ pow.py
@@ -3,8 +3,9 @@ import hashlib
 class Block():
     __slots__ = 'contents_hash', 'previous_hash'
     
-    def __init__(self, contents_hash=0, previous_hash=0):
-        # assert len(contents_hash) == 256 and len(previous_hash) == 256
+    def __init__(self, contents_hash='0'*256, previous_hash='0'*256):
+        assert type(contents_hash) == str and type(previous_hash) == str
+        assert len(contents_hash) == 256 and len(previous_hash) == 256
 
         self.contents_hash = contents_hash
         self.previous_hash = previous_hash 
@@ -21,7 +22,7 @@ class Block():
 
         if(previous):
             # calculate previous block header hash
-            previous_hash = str(previous.previous_hash)
+            previous_hash = previous.previous_hash
             block_header.previous_hash = hashlib.sha256(previous_hash.encode('utf-8')).hexdigest()
         else:
             # genesis has no previous. just use zeroed hash
@@ -30,9 +31,8 @@ class Block():
         block_header.contents_hash = hashlib.sha256(str(contents).encode('utf-8')).hexdigest()
         print(block_header.contents_hash)
 
-
 block_header = Block()
-previous = Block(0, '0x')
+previous = Block('0'*256, '0'*256)
 block_header.build_block(previous, 0)
 
 
